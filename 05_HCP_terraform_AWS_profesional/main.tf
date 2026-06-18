c5be376f9dfc838d50f4324ff74d467b19b91a2a
vbelmontetech-tech <vbelmontetech@gmail.com>
2026-06-18 18:25:45 +0200

    Fijar zona us-east-1a por capacidad

--- 05_HCP_terraform_AWS_profesional/main.tf
+++ 05_HCP_terraform_AWS_profesional/main.tf
@@ -1,8 +1,6 @@
-# Busca automáticamente la última AMI de Amazon Linux 2023 en tu región actual
 data "aws_ami" "amazon_linux" {
   most_recent = true
   owners      = ["amazon"]
-
   filter {
     name   = "name"
     values = ["al2023-ami-2023*-x86_64"]
@@ -10,8 +8,9 @@ data "aws_ami" "amazon_linux" {
 }
 
 resource "aws_instance" "servidor" {
-  ami           = data.aws_ami.amazon_linux.id
-  instance_type = "t2.micro"
+  ami               = data.aws_ami.amazon_linux.id
+  instance_type     = "t2.micro"
+  availability_zone = "us-east-1a" # Esta es la zona "ganadora" por capacidad
 
   tags = {
     Name = "Laboratorio-05-Profesional"
