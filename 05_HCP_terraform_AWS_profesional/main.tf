29e6faa89b2d74252be5a79d2dd0b6954db0f2aa
vbelmontetech-tech <vbelmontetech@gmail.com>
2026-06-18 18:07:15 +0200

    correcion AMI

--- 05_HCP_terraform_AWS_profesional/main.tf
+++ 05_HCP_terraform_AWS_profesional/main.tf
@@ -1,6 +1,17 @@
+# Busca automáticamente la última AMI de Amazon Linux 2023 en tu región actual
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023*-x86_64"]
+  }
+}
+
 resource "aws_instance" "servidor" {
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = var.tipo_instancia # Aquí conectamos con la variable
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t2.micro"
 
   tags = {
     Name = "Laboratorio-05-Profesional"
